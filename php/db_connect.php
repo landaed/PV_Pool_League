09ff7631cf4cdf3c696afe471bbb8b8c948fca3e
DESKTOP-TJFGPKO\Eli Landa <elanda@wooplagaming.com>
2024-01-24 15:44:44 -0800

    changed db charset

--- php/db_connect.php
+++ php/db_connect.php
@@ -3,7 +3,9 @@ define('DB_SERVER', 'localhost');
 define('DB_USERNAME', 'pvd001_eli');
 define('DB_PASSWORD', '>ZJkJ9i,)&FkD4M');
 define('DB_DATABASE', 'pvd001_new_pvd');
+
 $db = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
+$db->set_charset("utf8");
 if($db === false){
     die("ERROR: Could not connect. " . mysqli_connect_error());
 }
