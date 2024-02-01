b692a1bc833734deae3f1733a9060c03bc4df37f
DESKTOP-TJFGPKO\Eli Landa <elanda@wooplagaming.com>
2024-02-01 10:19:43 -0800

    added new email for internal use

--- php/submit_form.php
+++ php/submit_form.php
@@ -42,7 +42,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                // Recipients
                $mail->setFrom('noreply@pvpoolleagues.com', 'PV Pool Leagues');
                $mail->addAddress($captainEmail);     // Add a recipient for the captain
-               $mail->addAddress('eliplanda@gmail.com'); // Add a recipient for the company
 
                // Content
                $mail->isHTML(true);
@@ -56,6 +55,19 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                 "<img src='https://i.imgur.com/Xw7k2Gp.png' alt='PV Pool Leagues Logo' style='width:100px;'/>";
 
                $mail->AltBody = "Hello " . $captainName . ",\n\nThank you for registering your team, " . $teamName . ", in the PV Pool League.";
+
+               $mail->send();
+               // Clear all recipients and attachments for next email
+               $mail->clearAddresses();
+               $mail->clearAttachments();
+
+               // Recipient for the company
+               $mail->addAddress('eliplanda@gmail.com');
+
+               // Company's Email Content
+               $mail->Subject = 'New Team Registration - PV Pool League';
+               $mail->Body = "A new team has registered for the PV Pool League.<br><br><strong>Team Name:</strong> " . $teamName . "<br><strong>Division:</strong> " . $dayDivision . "<br><strong>Home Bar (First Pick):</strong> " . $homeBarFirst . "<br><strong>Home Bar (Second Pick):</strong> " . $homeBarSecond . "<br><strong>Captain Name:</strong> " . $captainName . "<br><strong>Captain Email:</strong> " . $captainEmail . "<br><strong>Captain Phone:</strong> " . $captainPhone . "<br><strong>Second Player:</strong> " . $player2 . "<br><strong>Registration Date:</strong> " . $registrationDate . "<br><br>Friar - League Coordinator";
+               $mail->AltBody = "A new team has registered for the PV Pool League.\n\nTeam Name: " . $teamName . "\nDivision: " . $dayDivision . "\nHome Bar (First Pick): " . $homeBarFirst . "\nHome Bar (Second Pick): " . $homeBarSecond . "\nCaptain Name: " . $captainName . "\nCaptain Email: " . $captainEmail . "\nCaptain Phone: " . $captainPhone . "\nSecond Player: " . $player2 . "\nRegistration Date: " . $registrationDate;
 
                $mail->send();
 
