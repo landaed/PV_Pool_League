2f9d028a525c9757a0691d3af45dca0f97746995
DESKTOP-TJFGPKO\Eli Landa <elanda@wooplagaming.com>
2024-09-03 14:23:48 -0700

    added printable pdf of teams

--- php/generate_pdf.php
+++ php/generate_pdf.php
@@ -7,7 +7,7 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-require_once __DIR__ . '/vendor/autoload.php';  // Corrected file path with forward slash
+require_once __DIR__ . '../vendor/autoload.php';  // Corrected file path with forward slash
 require_once 'db_connect.php';
 
 class MYPDF extends TCPDF {
